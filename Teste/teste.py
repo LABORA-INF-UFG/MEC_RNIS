9e58ad5f6d80e3a6263fbeec575072bb969cbda2
Kaique Matheus <mcunha@cpqd.com.br>
2023-11-17 18:26:48 -0300

    Teste RNIS-07.2

---
+++ Teste/teste.py
@@ -0,0 +1,31 @@
+import os
+import pandas as pd
+
+# Caminho onde os arquivos TXT estão localizados
+caminho_dos_arquivos = "/l/disk0/mcunha/Documentos/ufg/MEC_RNIS/Teste_1/"
+
+# Lista para armazenar os dados
+dados = []
+
+# Loop pelos arquivos no diretório
+for nome_arquivo in os.listdir(caminho_dos_arquivos):
+    if nome_arquivo.endswith(".txt"):
+        caminho_completo = os.path.join(caminho_dos_arquivos, nome_arquivo)
+        
+        # Adiciona uma linha em branco para separar os dados de cada arquivo
+        dados.append({'Nome do Arquivo': '', 'Valor': ''})
+
+        # Leitura do conteúdo do arquivo
+        with open(caminho_completo, 'r') as arquivo:
+            for linha in arquivo:
+                # Adiciona os dados à lista
+                dados.append({'Nome do Arquivo': nome_arquivo, 'Valor': linha.strip()})
+
+# Cria um DataFrame pandas com os dados
+df = pd.DataFrame(dados)
+
+# Salva o DataFrame em um arquivo Excel
+caminho_planilha = "/l/disk0/mcunha/Documentos/ufg/MEC_RNIS/Teste_1/planilha.xlsx"
+df.to_excel(caminho_planilha, index=False)
+
+print(f"Os dados foram salvos em {caminho_planilha}")
